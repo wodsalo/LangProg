57cdd2ab02646bc6fa499f5e373e5a9ef3bfbfe6
Rostislav Efimov <139411082+wodsalo@users.noreply.github.com>
2024-11-13 00:44:51 +0700

    Четвёртая версия класса help, добавили классы

---
+++ help.java
@@ -0,0 +1,7 @@
+class H {
+	void helpOn(int what) {
+	switch (choice) {
+	       	case '1':
+		       	System.out.println("Оператор \"if\":\n");
+			System.out.println("if(условие) оператор; или {блок операторов}");
+			System.out.println("else оператор; или {блок операторов}");
