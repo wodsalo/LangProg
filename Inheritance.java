31a54405a83a2cf9c35343e326100a29f1d6e60b
Rostislav Efimov <139411082+wodsalo@users.noreply.github.com>
2024-12-09 16:54:20 +0700

    Добавили абстрактные методы и классы, final, методы класса object

--- Inheritance.java
+++ Inheritance.java
@@ -1,6 +1,9 @@
-class TwoDShape {
+abstract class TwoDShape {
     private double width;
     private double height;
+    final int CONST1 = 1;
+    final int CONST2 = 2;
+    final int CONST3 = 3;
     String name;
     TwoDShape(double w, double h) {
         if(h>29.7)
@@ -34,10 +37,15 @@ class TwoDShape {
     void showDim() {
         System.out.println("Размер фигуры: " + width + " x " + height);
     }
+    /*
     double area() {
         System.out.println("Метод должен быть переопределен!");
         return 0.0;
     }
+         */
+    //метод который должен обязательно переопределится внутри подклассов
+    abstract double area();
+    abstract void getMsg();
 }
 
 class Triangle extends TwoDShape {
@@ -56,6 +64,9 @@ class Triangle extends TwoDShape {
     String showSuperName(){
         return super.name;
     }
+    final void getMsg(){
+        System.out.println("Сообщнеие от треугольника");
+    }
 }
 class ColorTriangle extends Triangle {
     String color;
@@ -66,6 +77,12 @@ class ColorTriangle extends Triangle {
     }
     void showColor(){
         System.out.println("Цвет " + color);
+    }
+/*     void getMsg(){
+        System.out.println("Сообщение от цветного треугольника");
+    }*/
+    public String toString(){
+        return "Цветной треугольник";
     }
 }
 class Rectangle extends TwoDShape {
@@ -84,18 +101,27 @@ class Rectangle extends TwoDShape {
     String showSuperName(){
         return super.name;
     }
+    
 }
+//запрет наследования класса X
+final class  X {
+    int x = 10;
+}
+/* 
+class Y extends X {
+    int y = 20;
+}*/
 
 class Inheritance {
-    public static void main(String[] args) {
-        TwoDShape fig1 = new TwoDShape(20.0, 30.0);
+    public static void main(String[] args) { 
+        TwoDShape fig1; // = new TwoDShape(20.0, 30.0); нельзя создать обьект обстрактного класса
         Triangle t1 = new Triangle("Закрашенный", 4.5, 7.0);
         ColorTriangle t2 = new ColorTriangle("Синий","Контурный", 3.5, 8.0);
         Rectangle r1 = new Rectangle("Сплошная линия", 5.0, 5.0); 
         Rectangle r2 = new Rectangle("Пунктирная линия", 5.0, 7.0);
         System.out.println("Информация об обьектах");
-        System.out.println("fig1: " + fig1.name);
-        fig1.showDim();
+        //System.out.println("fig1: " + fig1.name);
+        //fig1.showDim();
         System.out.println("t1: " + t1.name);
         t1.showStyle();
         System.out.println("Площадь: " + t1.area()); 
@@ -108,16 +134,17 @@ class Inheritance {
         //совместимость переменных родственных классов
         // переменная суперкласса может ссылатся на обьект любого подкласса
         System.out.println();
-        System.out.println("Ширина двумерной фигуры fig1: " + fig1.getWidth());
+        //System.out.println("Ширина двумерной фигуры fig1: " + fig1.getWidth());
         //при указании на обьект суперкласса выполнятеся метод суперкласса
-        fig1.area();
-        System.out.println("Имя фигуры: " + fig1.name); System.out.println();
+        //fig1.area();
+        //System.out.println("Имя фигуры: " + fig1.name); System.out.println();
 
         fig1 = t1;
         System.out.println("Ширина треугольника на который ссылается fig1: " + fig1.getWidth());
         //при указани на обьект подкласа выполняется вычисление площади для него
         System.out.println("Площадь треугольника " + fig1.area());
         System.out.println("Имя фигуры: " + fig1.name); System.out.println();
+        fig1.getMsg();
 
         fig1 = r1;
         System.out.println("Ширина прямоугольника на который ссылается fig1: " + fig1.getWidth());
@@ -129,7 +156,11 @@ class Inheritance {
         t2.showColor();
         System.out.println("Площадь цветного треугольника " + fig1.area());
         System.out.println("Имя фигуры: " + fig1.name);
+        fig1.getMsg();
 
+        System.out.println((fig1.CONST1 + fig1.CONST2 + fig1.CONST3));
+        //изменить переменную final нельльзя
+        //fig1.CONST1 = 333;
         //доступ к членам мподкласса для ссылочных переменных закрыт 
         //System.out.println(fig1.style);
 
@@ -139,6 +170,17 @@ class Inheritance {
         System.out.println(t1.width); //подкласс имеет доступ к переменным и методам суперкласса
         System.out.println(fig1.style);//супер класс не имеет доступа к переменным описаных в подклассе
         */   
+        //использлванеие класса Object
+        Object obj1;
+        System.out.println(t1.getClass());
+        System.out.println(t2.getClass());
+        System.out.println("Обьекты t1 и t2 эквивалентны: " + t1.equals(t2));
+        System.out.println("Хэш-код для t1 и t2: " + t1.hashCode() + t2.hashCode());
+        System.out.println("Описания для t1 и t2: " + t1.toString() + t2.toString());
+        t1 = t2;
+        System.out.println("Обьекты t1 и t2 эквивалентны: " + t1.equals(t2));
+
+
         /* 
         float n1 = 0x7F7FFFFF;
         int i1 = (int)n1;
